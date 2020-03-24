596cffbca7663f2964d69b820129d647ce8e6d39
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-24 13:59:16 -0400

    Added char count update on page ready

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,12 +7,15 @@ const MAX_TWEET_LENGTH = 140;
 
 $(document).ready(function() {
 
+  const $tweetTextarea  = $(".new-tweet output");
   const $charsRemaining = $(".new-tweet output");
 
-  $(".new-tweet textarea").bind("input change propertychange", function(_event) {
+  $tweetTextarea.on("input change propertychange", function(_event) {
     const charsRemaining = MAX_TWEET_LENGTH - $(this).val().length;
     $charsRemaining.html(charsRemaining).toggleClass("error", charsRemaining < 0);
   });
+
+  $tweetTextarea.trigger("change");
 
 });
 
