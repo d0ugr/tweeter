de81ca13958922650714cd57ab250d3df543ea04
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-24 13:49:22 -0400

    Changed negative char count styling to class definition

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -9,9 +9,9 @@ $(document).ready(function() {
 
   const $charsRemaining = $(".new-tweet output");
 
-  $(".new-tweet textarea").bind("input propertychange", function(_event) {
+  $(".new-tweet textarea").bind("input change propertychange", function(_event) {
     const charsRemaining = MAX_TWEET_LENGTH - $(this).val().length;
-    $charsRemaining.html(charsRemaining).css("color", (charsRemaining >= 0 ? "" : "red"));
+    $charsRemaining.html(charsRemaining).toggleClass("error", charsRemaining < 0);
   });
 
 });
