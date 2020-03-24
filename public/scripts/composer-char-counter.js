8f8d2920df727a487ea3a4e44211e66ef427acfc
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-24 12:01:10 -0400

    Added colour change on negative char count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -10,7 +10,8 @@ $(document).ready(function() {
   const $charsRemaining = $(".new-tweet output");
 
   $(".new-tweet textarea").bind("input propertychange", function(_event) {
-    $charsRemaining.html(MAX_TWEET_LENGTH - $(this).val().length);
+    const charsRemaining = MAX_TWEET_LENGTH - $(this).val().length;
+    $charsRemaining.html(charsRemaining).css("color", (charsRemaining >= 0 ? "" : "red"));
   });
 
 });
