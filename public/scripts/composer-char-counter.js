2a153068a6a90cf3149284fb0016adac7d72105d
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-25 17:52:20 -0400

    Fixed negative char count style

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
   $tweetTextarea.on("input change propertychange", function(_event) {
     const charsRemaining = MAX_TWEET_LENGTH - $(this).val().length;
-    $charsRemaining.html(charsRemaining).toggleClass("error", charsRemaining < 0);
+    $charsRemaining.html(charsRemaining).toggleClass("error-text", charsRemaining < 0);
   });
 
   $tweetTextarea.trigger("change");
