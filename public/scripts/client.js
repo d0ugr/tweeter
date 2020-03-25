7b5abae4a7f3b8afa7862df2b8a3f47202fb7073
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-25 16:06:54 -0400

    Implemented loadTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 
 
 const createTweetElement = function(tweet) {
@@ -63,24 +38,39 @@ const renderTweets = function(tweets) {
 
 };
 
+const loadTweets = function() {
+
+  $.ajax("/tweets", {
+    method:   "GET",
+    dataType: "JSON",
+    cache:    false
+  }).then(function(data, status, xhr) {
+    if (status !== "success") {
+      $("section.new-tweet").append(`<br>${status}: ${JSON.stringify(xhr, null, 2)}`);
+    } else {
+      renderTweets(data);
+    }
+  });
+
+};
+
 
 
 $(document).ready(function() {
 
-  renderTweets(tweetData);
+  // renderTweets(tweetData);
+  loadTweets();
 
   $("section.new-tweet form").on("submit", function(event) {
     event.preventDefault();
     $.ajax("/tweets", {
       method: "POST",
       data:   $(this).serialize()
-    }).then(function(err, status, xhr) {
-      if (err) {
-        $("section.new-tweet").append(err);
-      } else {
+    }).then(function(_data, status, xhr) {
+      if (status !== "success") {
         $("section.new-tweet textarea").val("");
-        $("section.new-tweet").append(`${status}: ${JSON.stringify(xhr, null, 2)}<br><br>`);
       }
+      $("section.new-tweet").append(`<br>${status}: ${JSON.stringify(xhr, null, 2)}`);
     });
   });
 
