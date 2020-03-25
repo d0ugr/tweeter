8e705a8639f37a5b48f9fcf76290b83e734126f0
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-25 14:50:15 -0400

    Implemented renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,23 +5,38 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name":    "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle":  "@SirIsaac"
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+
 
 const createTweetElement = function(tweet) {
 
   return $(`
     <article>
-      <header class="debug">
+      <header>
         <img src="${tweet.user.avatars}"><div class="name">${tweet.user.name}</div>
         <div class="handle">${tweet.user.handle}</div>
       </header>
@@ -39,11 +54,21 @@ const createTweetElement = function(tweet) {
 
 };
 
-const $tweet = createTweetElement(tweetData);
+const renderTweets = function(tweets) {
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  for (const tweet of tweets) {
+    $("section#tweets").append(createTweetElement(tweet));
+  }
+
+};
+
+
+
+$(document).ready(function() {
+
+  renderTweets(tweetData);
+
+});
 
 
 
