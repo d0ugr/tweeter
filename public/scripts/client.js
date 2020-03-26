6e887090dd17bcfb5cda3038dd2c23a8633da48c
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-25 21:31:47 -0400

    Refactored scrolling; moved stuff to document ready

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,89 +21,11 @@ const escapeText = function(text) {
 
 };
 
-const showNewTweetError = function(message, description) {
-
-  $("section.new-tweet .error h1").html((message     ? message     : DEFAULT_ERROR_MESSAGE));
-  $("section.new-tweet .error p") .html((description ? description : DEFAULT_ERROR_DESCRIPTION));
-  $("section.new-tweet .error").slideDown(ANIMATION_DURATION);
-
-};
-
-const hideNewTweetError = function() {
-
-  $("section.new-tweet .error").slideUp(ANIMATION_DURATION);
-
-};
-
-
-
-const createTweetElement = function(tweet) {
-
-  return $(`
-    <article>
-      <header>
-        <img src="${tweet.user.avatars}"><div class="name">${tweet.user.name}</div>
-        <div class="handle">${tweet.user.handle}</div>
-      </header>
-      <p>${escapeText(tweet.content.text)}</p>
-      <footer>
-        <span>${Math.floor((Date.now() - tweet.created_at) / 1000 / 86400)} days ago</span>
-        <div>
-          <img src="/images/profile-hex.png">
-          <img src="/images/profile-hex.png">
-          <img src="/images/profile-hex.png">
-        </div>
-      </footer>
-    </article>
-  `);
-
-};
-
-const renderTweets = function(tweets) {
-
-  const $tweetsSection = $("section#tweets");
-
-  if (!Array.isArray(tweets)) {
-    $tweetsSection.prepend(createTweetElement(tweets));
-  } else {
-    for (const tweet of tweets) {
-      $tweetsSection.prepend(createTweetElement(tweet));
-    }
-  }
-
-};
-
-const getTweets = function(callback) {
-
-  $.ajax("/tweets", {
-    method:   "GET",
-    dataType: "JSON",
-    cache:    false
-  }).then(function(data, status, xhr) {
-    if (status !== "success") {
-      callback(xhr, null);
-    } else {
-      callback(null, data);
-    }
-  });
-
-};
-
-const loadTweets = function() {
-
-  getTweets((err, data) => {
-    if (err) {
-      showNewTweetError(`Error loading tweets: ${JSON.stringify(err, null, 2)}`);
-    } else {
-      renderTweets(data);
-    }
-  });
-
-};
-
 
 
 $(document).ready(function() {
+
+
 
   const $window      = $(window);
   const $header      = $("header");
@@ -113,8 +35,119 @@ $(document).ready(function() {
   const $textarea    = $("section.new-tweet textarea");
   const $error       = $("section.new-tweet .error");
 
-  loadTweets();
+
+
+  const showScrollToTop = function() {
+
+    if ($window.scrollTop() > $header.outerHeight()) {
+      if (!$scrollToTop.is(":visible")) {
+        $scrollToTop.fadeIn(ANIMATION_DURATION);
+      }
+    } else {
+      if ($scrollToTop.is(":visible")) {
+        $scrollToTop.fadeOut(ANIMATION_DURATION);
+      }
+    }
+
+  };
+
+  const scrollToTop = function() {
+
+    window.scroll({
+      top:      0,
+      left:     0,
+      behavior: "smooth"
+    });
+
+  };
+
+  const showNewTweetError = function(message, description) {
+
+    $("section.new-tweet .error h1").html((message     ? message     : DEFAULT_ERROR_MESSAGE));
+    $("section.new-tweet .error p") .html((description ? description : DEFAULT_ERROR_DESCRIPTION));
+    $("section.new-tweet .error").slideDown(ANIMATION_DURATION);
+
+  };
+
+  const hideNewTweetError = function() {
+
+    $("section.new-tweet .error").slideUp(ANIMATION_DURATION);
+
+  };
+
+
+
+  const createTweetElement = function(tweet) {
+
+    return $(`
+      <article>
+        <header>
+          <img src="${tweet.user.avatars}"><div class="name">${tweet.user.name}</div>
+          <div class="handle">${tweet.user.handle}</div>
+        </header>
+        <p>${escapeText(tweet.content.text)}</p>
+        <footer>
+          <span>${Math.floor((Date.now() - tweet.created_at) / 1000 / 86400)} days ago</span>
+          <div>
+            <img src="/images/profile-hex.png">
+            <img src="/images/profile-hex.png">
+            <img src="/images/profile-hex.png">
+          </div>
+        </footer>
+      </article>
+    `);
+
+  };
+
+  const renderTweets = function(tweets) {
+
+    const $tweetsSection = $("section#tweets");
+
+    if (!Array.isArray(tweets)) {
+      $tweetsSection.prepend(createTweetElement(tweets));
+    } else {
+      for (const tweet of tweets) {
+        $tweetsSection.prepend(createTweetElement(tweet));
+      }
+    }
+
+  };
+
+  const getTweets = function(callback) {
+
+    $.ajax("/tweets", {
+      method:   "GET",
+      dataType: "JSON",
+      cache:    false
+    }).then(function(data, status, xhr) {
+      if (status !== "success") {
+        callback(xhr, null);
+      } else {
+        callback(null, data);
+      }
+    });
+
+  };
+
+  const loadTweets = function() {
+
+    getTweets((err, data) => {
+      if (err) {
+        showNewTweetError(`Error loading tweets: ${JSON.stringify(err, null, 2)}`);
+      } else {
+        renderTweets(data);
+      }
+    });
+
+  };
+
+
+
   $newTweet.hide();
+  showScrollToTop();
+  loadTweets();
+
+
 
   $textarea.on("keydown", function(event) {
     if (event.keyCode === KeyboardEvent.DOM_VK_RETURN) {
@@ -169,15 +202,7 @@ $(document).ready(function() {
   });
 
   $window.on("scroll", function(_event) {
-    if ($window.scrollTop() > $header.outerHeight()) {
-      if (!$scrollToTop.is(":visible")) {
-        $scrollToTop.fadeIn(ANIMATION_DURATION);
-      }
-    } else {
-      if ($scrollToTop.is(":visible")) {
-        $scrollToTop.fadeOut(ANIMATION_DURATION);
-      }
-    }
+    showScrollToTop();
   });
 
   $("nav #nav_tweet").on("click", function(event) {
@@ -189,17 +214,24 @@ $(document).ready(function() {
       $textarea.blur();
       $newTweet.slideUp(ANIMATION_DURATION);
     }
-    window.scroll({
-      top:      0,
-      left:     0,
-      behavior: "smooth"
-    });
+    scrollToTop();
+  });
+
+  $("nav #scroll-to-top").on("click", function(event) {
+    event.preventDefault();
+    if (!$newTweet.is(":visible")) {
+      $newTweet.slideDown(ANIMATION_DURATION);
+    }
+    $textarea.focus();
+    scrollToTop();
   });
 
   $error.on("click", function(event) {
     event.preventDefault();
     hideNewTweetError();
   });
+
+
 
 });
 
